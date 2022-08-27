a87e1695f1f301200ae337ca9d570b0dd27ac0eb
FR <f.rezaee@email.kntu.ac.ir>
2022-08-27 11:50:48 +0430

    pass != pass trouble!

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,24 @@
-from django.db import models  # noqa
+from django.contrib.auth.models import(
+    PermissionsMixin,
+    BaseUserManager,
+    AbstractBaseUser
+)
+from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=320, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
