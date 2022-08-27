cce4089469c3e7a89f745ff4f1272fc9589f6247
FR <f.rezaee@email.kntu.ac.ir>
2022-08-27 13:24:19 +0430

    fixed2

--- app/core/models.py
+++ app/core/models.py
@@ -7,8 +7,22 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(self._db)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return user
+
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User must have an email address.")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(self._db)
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,3 +12,18 @@ class ModelTest(TestCase):
         user = get_user_model().objects.create_user(email=email, password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password, password)
+
+    def test_new_user_normalized_email(self):
+        sample = [
+            ['Test@example.com', 'Test@example.com'],
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['test2@example.COM', 'test2@example.com'],
+            ['test3@EXAMPLE.COM', 'test3@example.com']
+        ]
+        for email, expected in sample:
+            user = get_user_model().objects.create_user(email=email, password='password123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_emailess_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', password='45')
