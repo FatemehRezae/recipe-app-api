3ebd67889380049294bfa42d941a308d15263e93
FR <f.rezaee@email.kntu.ac.ir>
2022-08-30 12:47:36 +0430

    fixed 2

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,13 +5,13 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
-class ModelTest(TestCase):
+class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = 'test@example.com'
         password = 'password123'
         user = get_user_model().objects.create_user(email=email, password=password)
         self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password, password)
+        self.assertTrue(user.check_password(password))
 
     def test_new_user_normalized_email(self):
         sample = [
@@ -27,3 +27,13 @@ class ModelTest(TestCase):
     def test_new_user_emailess_raises_error(self):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', password='45')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
