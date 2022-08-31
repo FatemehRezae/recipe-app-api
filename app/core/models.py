fd4db9fbaa72868f5ae755f579e662d959451780
FR <f.rezaee@email.kntu.ac.ir>
2022-08-31 12:25:52 +0430

    ?

--- app/core/models.py
+++ app/core/models.py
@@ -7,16 +7,10 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         return user
 
     def create_user(self, email, password=None, **extra_fields):
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -5,4 +5,22 @@ from django.test import Client
 
 
 class AdminSiteTest(TestCase):
-    pass
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model.object.create_superuser(
+            email='admin@example.com',
+            password='passWord123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model.object.create_user(
+            email='user@example.com',
+            password='pass123',
+            name='Test User'
+        )
+
+    def test_users_list(self):
+        url = reverse('admin:core_user_changelist')
+        res = self.client(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
