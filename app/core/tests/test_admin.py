428a665ccefce47bd43132c743ca4831a42d9dd7
FR <f.rezaee@email.kntu.ac.ir>
2022-08-31 16:10:44 +0430

    ?

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -28,3 +28,9 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """test the edit user page works."""
+        url = reverse('admin:core_user_change', agrs=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
